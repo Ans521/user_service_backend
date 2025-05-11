ffb1878ad0bfd0bc03366b6586a28a435cba0bc7
Ansh Sharma <anshattrii12@gmail.com>
2025-05-11 18:29:48 +0530

    completed icon and image for subcategory structure and api's

--- user_provider/src/controllers/providerController.ts
+++ user_provider/src/controllers/providerController.ts
@@ -1,41 +1,86 @@
 import { ServiceProvider } from "../models/serviceProvider";
 import { Types } from "mongoose";
-
+import { SubCategory } from "../models/subCategory";
+ 
 export const uploadImages = async (req : any,  res : any) => {
     try {
         const { id, isEmployeeLogin }  = req.user;
-
+ 
         if(!isEmployeeLogin || !id) {
             return res.status(401).json({ success: false, message: 'Unauthorized' });
         }
-
+ 
         const imageUrlObj = req.files;
-
+ 
         if (!imageUrlObj) {
             return res.status(400).json({ success: false, message: 'uploadImageUrl is required' });
         }
         const imageUrls = imageUrlObj.map((file : any) =>  `http://13.202.163.238:4000/uploads/${file.filename}`);
-
+ 
         if (imageUrls.length === 0) {
             return res.status(400).json({ success: false, message: 'No images uploaded' });
         }
-
+ 
         const  phoneNoId = new Types.ObjectId(String(id));
         const galleryImagesCheck : any = await ServiceProvider.findOne({phoneNo : phoneNoId}).select('galleryImages');
-
+ 
         // if(galleryImagesCheck?.galleryImages?.length >= 6) {
         //     return res.status(400).json({ success: false, message: 'You can only upload 6 images' });
         // }
-
+ 
         const providerImages : any = await ServiceProvider.findOneAndUpdate(
             {phoneNo : phoneNoId},
             {$push : { galleryImages: {$each : imageUrls} }},
             { new: true}
         )
-
+ 
         return res.status(200).json({ success: true, message : 'Images uploaded successfully', data: providerImages });
     } catch (error) {
         console.error('Error fetching user:', error);
         return res.status(500).json({ success: false, message: 'Internal Server Error' });
     }
 };
+
+export const addSpecialCategory = async (req : any, res : any) => {
+    try{
+        console.log("req.body", req.body)
+        const { data } = req.body;
+        if(!data || data.length === 0) {
+            return res.status(400).json({ success: false, message: 'data is required' });
+        }
+        const operations = data.map((item : any) => {
+            return {
+                updateOne : {
+                    filter : {_id : item.subcategoryId},
+                    update : {
+                        $set : {
+                            iconImage : item.iconImage,
+                            specialCategory : true
+                        }
+                    }
+                }
+            }
+        })
+ 
+        const result : any = await SubCategory.bulkWrite(operations);
+        console.log("result", result);
+        return res.status(200).json({ success: true, message : 'subcat added successfully' });
+    }catch(error){
+        console.error('Error fetching user:', error);
+        return res.status(500).json({ success: false, message: 'Internal Server Error' });
+    }
+}
+ 
+export const getAddedSpecialCateogory = async (req : any, res : any) => {
+    try{
+        const data = await SubCategory.find({specialCategory : true}).select('-__v');
+        if(!data || data.length === 0) {
+            return res.status(400).json({ success: false, message: 'No data found' });
+        }
+        return res.status(200).json({ success: true, data });
+    }catch(error){
+        console.error('Error fetching user:', error);
+        return res.status(500).json({ success: false, message: 'Internal Server Error' });
+    }
+}
+ 
--- user_provider/src/models/subCategory.ts
+++ user_provider/src/models/subCategory.ts
@@ -1,8 +1,26 @@
 import mongoose from "mongoose";
-
+ 
 const subcategorySchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category' },
+  name: {
+    type: String,
+    required: true
+  },
+  category: {
+  type: mongoose.Schema.Types.ObjectId,
+  ref: 'Category'
+  },
+  image : {
+    type : String,
+  },
+  iconImage : {
+    type : String
+  },
+  specialCategory : {
+    type : Boolean,
+    default : false
+  }
 });
-
+ 
 export const SubCategory = mongoose.model('SubCategory', subcategorySchema);
+ 
+ 
