fb3defa27e6f3d25886c5829b99673b2ea67db3e
Ansh Sharma <anshattrii12@gmail.com>
2025-06-25 22:54:30 +0530

    small changes

--- user_provider/src/utils/redisUtils.ts
+++ user_provider/src/utils/redisUtils.ts
@@ -1,27 +1,27 @@
-import admin from "firebase-admin";
-import serviceAccount from "../public/local-professionals-firebase-adminsdk-fbsvc-31b56d4ffb.json";
+// import admin from "firebase-admin";
+// import serviceAccount from "../public/local-professionals-firebase-adminsdk-fbsvc-31b56d4ffb.json";
 
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-});
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+// });
 
-export const sendPush = async (message : string, name : any, deviceToken : any) => {
+// export const sendPush = async (message : string, name : any, deviceToken : any) => {
     
-    const fcmToken = deviceToken;
+//     const fcmToken = deviceToken;
     
-      try {
-        await admin.messaging().send({
-            token: fcmToken,
-            notification: {
-              title: `New message from the ${name}`,
-              body: `${message}`,
-            },
-        });
+//       try {
+//         await admin.messaging().send({
+//             token: fcmToken,
+//             notification: {
+//               title: `New message from the ${name}`,
+//               body: `${message}`,
+//             },
+//         });
 
-  } catch (error) {
-    console.error('Error sending push:', error);
-  }
-}
+//   } catch (error) {
+//     console.error('Error sending push:', error);
+//   }
+// }
 
     
