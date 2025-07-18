8007dc2ede3a39e385318e069354f3d256183354
Ansh Sharma <anshattrii12@gmail.com>
2025-07-18 09:52:36 +0530

    Doing changes for the recent connected and enquiry for user

--- user_provider/src/models/user.ts
+++ user_provider/src/models/user.ts
@@ -10,15 +10,7 @@ const userSchema = new mongoose.Schema({
       type: String,
       default: "not provided"
    },
-   userMsg: {
-      type: [{
-         receiverId: { type: mongoose.Schema.Types.ObjectId, ref: 'ServiceProvider' },
-         messages: [new mongoose.Schema({
-            message: { type: String },
-            timeStamp: { type: Date, default: Date.now },
-         }, { _id: false })]
-      }],
-   }
+ 
 });
 
 export const User = Base.discriminator("User", userSchema);
